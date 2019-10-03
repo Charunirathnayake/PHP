9b921a8cd195f7499b99ba1271e209c0e554d23d
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-10-03 15:14:49 +0530

    Learning objectives

---
+++ special_chars_function.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<a href="http://www.google.com">
+		<?php 
+			$str="<click here>";
+			echo htmlspecialchars($str);
+		?>
+	</a>
+</body>
+</html>
