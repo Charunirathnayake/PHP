2fda2ff94e4bafecc8ed4af8394341b295aa9e00
Charunirathnayake <charunirathnayake1995@gmail.com>
2019-10-09 11:26:54 +0530

    Learning objectives of PHP

---
+++ form_destination.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+		<?php 
+		if(isset($_POST['submit'])){
+			echo "Name: {$_POST['Name']} <br> ";
+			echo "Age:{$_POST['Age']} <br>";
+			echo "Gender:{$_POST['Gen']}<br>";
+			echo "Email:{$_POST['Email']}<br>";
+
+
+		}
+		else{
+			echo "Form was not submitted.";
+		}
+		?>
+</body>
+</html>
---
+++ form_source.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<form action="form_destination.php" method="post">
+	Name:	<input type="text" name="Name"/><br><br>
+	Age:&nbsp;&nbsp;&nbsp;<input type="text" name="Age"/><br><br>
+	Gender:&nbsp;Male<input type="radio" name="Gen" value="Male"/>
+	Female<input type="radio" name="Gen" value="Female"/><br><br>
+	Email:&nbsp;<input type="text" name="Email"/><br><br>
+	<input type="submit" name="submit" value="Submit">
+	</form>
+</body>
+</html>
